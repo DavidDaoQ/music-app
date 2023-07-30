21a783ad3c9fdeb65e5629bd98eb60284c7ca886
Dao Dang Quang <dquang.haiphong@gmail.com>
2023-07-30 23:37:03 +0700

    mp3

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
     <div className="h-24 bg-slate-900 text-white text-center leading-[6rem] text-3xl">
       <i className="fab fa-spotify mr-4"></i>
-      spotify
+      Music player
     </div>
   );
 }
